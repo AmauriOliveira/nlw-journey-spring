3b25c7e4abdcfd103e8aaaa2dbbc5e3bcd6a2471
Amauri Oliveira <amauri.oliveira.tokenlab@segurosunimed.com.br>
2024-07-09 15:34:32 -0300

    Inicio do projeto

---
+++ src/test/java/com/oliveira/amauri/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.amauri.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
