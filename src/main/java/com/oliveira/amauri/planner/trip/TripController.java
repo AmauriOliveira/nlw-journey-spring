db551cc8e818762e688dd02e697ebb0c3ccee3d7
Amauri Oliveira <amauriibate32@hotmail.com>
2024-07-09 21:59:23 -0300

    Criação do endpoint get trips by uuid

--- src/main/java/com/oliveira/amauri/planner/trip/TripController.java
+++ src/main/java/com/oliveira/amauri/planner/trip/TripController.java
@@ -3,10 +3,10 @@ package com.oliveira.amauri.planner.trip;
 import com.oliveira.amauri.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -28,5 +28,13 @@ public class TripController {
         this.participantService.registerParticipantsToEvent(tripPayload.emails_to_invite(), newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripsDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        return trip.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
