58d60ce45e8047bae02f8b7bc00750168672c1c8
Amauri Oliveira <amauriibate32@hotmail.com>
2024-07-10 23:53:24 -0300

    Criação da Tabela activities e a Entity Activity

---
+++ src/main/java/com/oliveira/amauri/planner/activity/Activity.java
@@ -0,0 +1,33 @@
+package com.oliveira.amauri.planner.activity;
+
+import com.oliveira.amauri.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String tittle;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
