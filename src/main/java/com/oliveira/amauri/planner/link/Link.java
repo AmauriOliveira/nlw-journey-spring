08f5a94e7de1e376e03276aaff8dbd786e4f9dfe
Amauri Oliveira <amauriibate32@hotmail.com>
2024-07-11 08:56:32 -0300

    Criação do migration Links e sua Entity e Repository

---
+++ src/main/java/com/oliveira/amauri/planner/link/Link.java
@@ -0,0 +1,35 @@
+package com.oliveira.amauri.planner.link;
+
+import com.oliveira.amauri.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private java.util.UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
